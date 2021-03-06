8b46ae0aff6202ebe546d0ea44410f365510741d
stnolting <stnolting@gmail.com>
2021-03-06 21:58:35 +0100

    added latest tool chain to test workflow

--- .ci/test.sh
+++ .ci/test.sh
@@ -30,3 +30,14 @@ ls -al
 mkdir $GITHUB_WORKSPACE/$TEST.tmp
 tar -xzf $TEST.tar.gz -C $GITHUB_WORKSPACE/$TEST.tmp/
 $GITHUB_WORKSPACE/$TEST.tmp/bin/$TOOLCHAIN-gcc -v
+
+echo ""
+
+RELEASE=rv32i-2.0.0
+TEST=riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib
+echo "Testing $RELEASE : $TEST/$TOOLCHAIN..."
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$TEST.tar.gz
+ls -al
+mkdir $GITHUB_WORKSPACE/$TEST.tmp
+tar -xzf $TEST.tar.gz -C $GITHUB_WORKSPACE/$TEST.tmp/
+$GITHUB_WORKSPACE/$TEST.tmp/bin/$TOOLCHAIN-gcc -v
