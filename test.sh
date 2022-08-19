b63550b5a79183c8fa7909a93d5012752ab6f0d2
stnolting <stnolting@gmail.com>
2022-08-19 18:38:43 +0200

    clean-up test script

--- test.sh
+++ test.sh
@@ -11,17 +11,6 @@ echo ""
 
 
 # Toolchain to be used
-RELEASE=rv32i-1.0.0
-TEST=riscv32-unknown-elf.gcc-10.1.0.rv32i.ilp32.newlib
-echo "Testing $RELEASE : $TEST/$TOOLCHAIN..."
-wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$TEST.tar.gz
-ls -al
-mkdir $GITHUB_WORKSPACE/$TEST.tmp
-tar -xzf $TEST.tar.gz -C $GITHUB_WORKSPACE/$TEST.tmp/
-$GITHUB_WORKSPACE/$TEST.tmp/bin/$TOOLCHAIN-gcc -v
-
-echo ""
-
 RELEASE=rv32e-1.0.0
 TEST=riscv32-unknown-elf.gcc-10.1.0.rv32e.ilp32e.newlib
 echo "Testing $RELEASE : $TEST/$TOOLCHAIN..."
