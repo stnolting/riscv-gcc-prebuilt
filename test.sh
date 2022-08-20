bc62ee32307e207c8a3cfcaa19421ad0c7d35a8c
stnolting <stnolting@gmail.com>
2022-08-20 17:25:11 +0200

    add newest rv32i toolchain

--- test.sh
+++ test.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 
-# Abort if any command returns != 0
 set -e
 
-# Decompress and test
 echo ""
 
-TOOLCHAIN=riscv32-unknown-elf
+PREFIX=riscv32-unknown-elf
 RELEASE=rv32e-1.0.0
-TEST=riscv32-unknown-elf.gcc-10.1.0.rv32e.ilp32e.newlib
-echo "Testing $RELEASE : $TEST/$TOOLCHAIN..."
-wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$TEST.tar.gz
+ARCHIVE=riscv32-unknown-elf.gcc-10.1.0.rv32e.ilp32e.newlib
+echo "Checking $RELEASE : $ARCHIVE/$PREFIX..."
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$ARCHIVE.tar.gz
 ls -al
-mkdir $GITHUB_WORKSPACE/$TEST.tmp
-tar -xzf $TEST.tar.gz -C $GITHUB_WORKSPACE/$TEST.tmp/
-$GITHUB_WORKSPACE/$TEST.tmp/bin/$TOOLCHAIN-gcc -v
+mkdir $GITHUB_WORKSPACE/$ARCHIVE.tmp
+tar -xzf $ARCHIVE.tar.gz -C $GITHUB_WORKSPACE/$ARCHIVE.tmp/
+$GITHUB_WORKSPACE/$ARCHIVE.tmp/bin/$PREFIX-gcc -v
 
 echo ""
 
-TOOLCHAIN=riscv32-unknown-elf
+PREFIX=riscv32-unknown-elf
 RELEASE=rv32i-2.0.0
-TEST=riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib
-echo "Testing $RELEASE : $TEST/$TOOLCHAIN..."
-wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$TEST.tar.gz
+ARCHIVE=riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib
+echo "Checking $RELEASE : $ARCHIVE/$PREFIX..."
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$ARCHIVE.tar.gz
 ls -al
-mkdir $GITHUB_WORKSPACE/$TEST.tmp
-tar -xzf $TEST.tar.gz -C $GITHUB_WORKSPACE/$TEST.tmp/
-$GITHUB_WORKSPACE/$TEST.tmp/bin/$TOOLCHAIN-gcc -v
+mkdir $GITHUB_WORKSPACE/$ARCHIVE.tmp
+tar -xzf $ARCHIVE.tar.gz -C $GITHUB_WORKSPACE/$ARCHIVE.tmp/
+$GITHUB_WORKSPACE/$ARCHIVE.tmp/bin/$PREFIX-gcc -v
 
 echo ""
 
-TOOLCHAIN=riscv64-unknown-elf
+PREFIX=riscv64-unknown-elf
 RELEASE=rv64imc-3.0.0
-TEST=riscv64-unknown-elf.gcc-12.1.0
-echo "Testing $RELEASE : $TEST/$TOOLCHAIN..."
-wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$TEST.tar.gz
+ARCHIVE=riscv64-unknown-elf.gcc-12.1.0
+echo "Checking $RELEASE : $ARCHIVE/$PREFIX..."
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$ARCHIVE.tar.gz
 ls -al
-mkdir $GITHUB_WORKSPACE/$TEST.tmp
-tar -xzf $TEST.tar.gz -C $GITHUB_WORKSPACE/$TEST.tmp/
-$GITHUB_WORKSPACE/$TEST.tmp/bin/$TOOLCHAIN-gcc -v
+mkdir $GITHUB_WORKSPACE/$ARCHIVE.tmp
+tar -xzf $ARCHIVE.tar.gz -C $GITHUB_WORKSPACE/$ARCHIVE.tmp/
+$GITHUB_WORKSPACE/$ARCHIVE.tmp/bin/$PREFIX-gcc -v
+
+echo ""
+
+PREFIX=riscv32-unknown-elf
+RELEASE=rv32i-4.0.0
+ARCHIVE=riscv32-unknown-elf.gcc-12.1.0
+echo "Checking $RELEASE : $ARCHIVE/$PREFIX..."
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/$RELEASE/$ARCHIVE.tar.gz
+ls -al
+mkdir $GITHUB_WORKSPACE/$ARCHIVE.tmp
+tar -xzf $ARCHIVE.tar.gz -C $GITHUB_WORKSPACE/$ARCHIVE.tmp/
+$GITHUB_WORKSPACE/$ARCHIVE.tmp/bin/$PREFIX-gcc -v
